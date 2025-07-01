997fa598428f9bc6789a82d514e642e791f7cefd
Sivatheevan1224 <sivatheevan1224@gmail.com>
2025-07-02 02:05:12 +0530

    session change

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,6 +6,7 @@ const AuthContext = createContext();
 export function useAuth() {
   return useContext(AuthContext);
 }
+//
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
